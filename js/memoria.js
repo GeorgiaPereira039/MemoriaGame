67b723355bcddf3b9688ffd3d06bdbdab17e8660
Code Hangen <111701513+codehangen@users.noreply.github.com>
2023-01-12 18:49:10 -0300

    adicionando pontuacao e save point
    se o participante do jogo conseguir memorizar as 32 cartas do jogo, esse participante vencerá o jogo
    nesse caso, a pontuação será de 32 pontos de cada jogo
    então se o participante vencer 2, será de 64 pontos - e sucessivamente a medida de 32 pontos por jogo ganho
    pensei inicialmente em utilizar o valor de 1 ponto de cada jogo, mas ficaria chato demais ter apenas 1 ponto por jogo
    32 pontos por jogo é mais emocionante penso eu

--- js/memoria.js
+++ js/memoria.js
@@ -1,5 +1,4 @@
 const tabuleiro = document.querySelector('.tabuleiro');
-
 const personagens = [
 	'kadabra',
 	'mimikyu',
@@ -19,6 +18,13 @@ const personagens = [
 	'psyduck',
 ];
 
+const setGamePoint = (statusGame, codeStatusGame) => {
+  return localStorage.setItem(statusGame, codeStatusGame);
+};
+
+const getGamePoint = (codeStatusGame) => {
+  return localStorage.getItem(codeStatusGame);
+};
 
 const createElement = (tag, className) => {
 	const element = document.createElement(tag);
@@ -31,11 +37,29 @@ let segundaCarta = '';
 
 const fimDeJogo = () => {
 	const cartaInativa = document.querySelectorAll('.carta-inativa');
-
-		if (cartaInativa.length === 34) {
-			alert ('Parabéns, você conseguiu!');
-			playAgain();
+	const para = document.createElement("p");
+		if (cartaInativa.length === 32) { // 34????
+			para.innerText = cartaInativa.length;
+			document.body.appendChild(para);
+			//alert('Parabéns, você conseguiu'); //console.log('Parabéns, você conseguiu!'); 
+			//setGamePoint('statusGame','aceito');
+			//setGamePoint('pontuacao', cartaInativa.length);
+			//playAgain();
 		}
+		//const statusGame = getGamePoint("statusGame"); // console.log(estadoJogo);
+		//const pontuacao = getGamePoint("pontuacao"); // console.log(pontuacao);
+		//const para = document.createElement("p");
+		//if (statusGame=='aceito') {
+		//console.log('statusGame?', statusGame);
+		// exibindo pontuacao
+		// console.log('pontuacao?', pontuacao); // 32 cartas = 32 pontos por jogo
+		//if(pontuacao==32) {
+		// exibindo pontuacao
+		//console.log('pontuacao?', pontuacao); // 32 cartas = 32 pontos por jogo
+		//para.innerText = pontuacao;
+		//document.body.appendChild(para);
+		//}
+		//}
 }
 
 function playAgain() {
@@ -49,47 +73,32 @@ function restart () {
 const checarCarta = () => {
 	 const primeiroPersonagem = primeiraCarta.getAttribute('data-personagens');
 	 const segundoPersonagem = segundaCarta.getAttribute('data-personagens');
-
 	 if (primeiroPersonagem === segundoPersonagem) {
-
 			primeiraCarta.firstChild.classList.add('carta-inativa');
 			segundaCarta.firstChild.classList.add('carta-inativa');
-			
 			primeiraCarta = '';
 			segundaCarta = '';
-
 			fimDeJogo();
-
 	 } else {
 		setTimeout(() => {
-
 			primeiraCarta.classList.remove('revele-carta');
 			segundaCarta.classList.remove('revele-carta');
-
 			primeiraCarta = '';
 			segundaCarta = '';
-
 		}, 500);
-
 	 }
 }
 
 const reveleCarta = ({target}) => {
-	
 	 if (target.parentNode.className.includes('revele-carta')) {
   	  return;
   }
-
 	if (primeiraCarta === '') {
-
 		target.parentNode.classList.add('revele-carta');
 		primeiraCarta = target.parentNode;
-	
 	} else if (segundaCarta === '') {
-
 		target.parentNode.classList.add('revele-carta');
 		segundaCarta = target.parentNode;
-
 		checarCarta();
 	}
 }
@@ -98,25 +107,17 @@ const createCarta = (personagens) => {
 	const carta = createElement('div', 'carta');
 	const front = createElement('div', 'face front');
 	const back = createElement('div', 'face back');
-
 	front.style.backgroundImage = `url('./css/image/${personagens}.png')`;
-
 	carta.appendChild(front);
 	carta.appendChild(back);
-
 	carta.addEventListener('click', reveleCarta);
 	carta.setAttribute('data-personagens', personagens)
-	
-
 	return carta;
 }
 
 const loadGame = () => {
-
 	const duplicatepersonagens = [ ...personagens, ...personagens]
-
 	const shufflyArray = duplicatepersonagens.sort(() => Math.random() - 0.5);
-
 	shufflyArray.forEach((personagens) => {
 		const carta = createCarta(personagens);
 		tabuleiro.appendChild(carta);

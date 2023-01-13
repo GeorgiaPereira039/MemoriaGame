e996aa2ecc6b5419adf12f894c66cabb86dd5d72
Code Hangen <111701513+codehangen@users.noreply.github.com>
2023-01-13 05:19:09 -0300

    algoritmo atualizado

--- js/memoria.js
+++ js/memoria.js
@@ -1,24 +1,5 @@
 const tabuleiro = document.querySelector('.tabuleiro');
-
-const personagens = [
-	'kadabra',
-	'mimikyu',
-	'slowbro',
-	'espeon',
-	'murkrow',
-	'togepi',
-	'wobbuffet',
-	'gloom',
-	'nickit',
-	'croagunk',
-	'glastly',
-	'drowzee',
-	'drifloon',
-	'ralts',
-	'spinda',
-	'psyduck',
-];
-
+const personagens = ['kadabra', 'mimikyu', 'slowbro', 'espeon', 'murkrow', 'togepi', 'wobbuffet', 'gloom', 'nickit', 'croagunk', 'glastly', 'drowzee', 'drifloon', 'ralts', 'spinda', 'psyduck'];
 
 const createElement = (tag, className) => {
 	const element = document.createElement(tag);
@@ -31,9 +12,9 @@ let segundaCarta = '';
 
 const fimDeJogo = () => {
 	const cartaInativa = document.querySelectorAll('.carta-inativa');
-
-		if (cartaInativa.length === 34) {
-			alert ('Parabéns, você conseguiu!');
+		if (cartaInativa.length === 32) { // 34????
+			console.log('Parabéns, você conseguiu!'); 
+			setGamePoint('pontuacao', cartaInativa.length);
 			playAgain();
 		}
 }
@@ -49,77 +30,78 @@ function restart () {
 const checarCarta = () => {
 	 const primeiroPersonagem = primeiraCarta.getAttribute('data-personagens');
 	 const segundoPersonagem = segundaCarta.getAttribute('data-personagens');
-
 	 if (primeiroPersonagem === segundoPersonagem) {
-
 			primeiraCarta.firstChild.classList.add('carta-inativa');
 			segundaCarta.firstChild.classList.add('carta-inativa');
-			
 			primeiraCarta = '';
 			segundaCarta = '';
-
 			fimDeJogo();
-
 	 } else {
 		setTimeout(() => {
-
 			primeiraCarta.classList.remove('revele-carta');
 			segundaCarta.classList.remove('revele-carta');
-
 			primeiraCarta = '';
 			segundaCarta = '';
-
 		}, 500);
-
 	 }
 }
 
 const reveleCarta = ({target}) => {
-	
 	 if (target.parentNode.className.includes('revele-carta')) {
   	  return;
   }
-
 	if (primeiraCarta === '') {
-
 		target.parentNode.classList.add('revele-carta');
 		primeiraCarta = target.parentNode;
-	
 	} else if (segundaCarta === '') {
-
 		target.parentNode.classList.add('revele-carta');
 		segundaCarta = target.parentNode;
-
 		checarCarta();
 	}
 }
 
-const createCarta = (personagens) => {
+// upload-card or local-card or random-card for back-card
+const imageArray = [
+"https://images.unsplash.com/photo-1508185159346-bb1c5e93ebb4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=55cf14db6ed80a0410e229368963e9d8&auto=format&fit=crop&w=1900&q=80", 
+"./css/back.png", 
+"./css/back1.jpg", 
+//`url('./css/image/${personagens}.png')`
+];
+
+const randomNum = Math.floor(Math.random() * imageArray.length);
+const selectTypeCardNow = (selectObject)=> {
+	if(selectObject.value === '0'){
+		 return `url(`+imageArray[0]+`)`;
+	}
+	if(selectObject.value === '1'){
+		 return `url(`+imageArray[randomNum]+`)`;
+	}
+	if(selectObject.value === '2'){
+		 return `url(`+imageArray[1]+`)`;
+	}
+	if(selectObject.value === '3'){
+		 return `url(`+imageArray[2]+`)`;
+	}
+}
+
+const createCarta = (personagens, selectTypeCard) => {
 	const carta = createElement('div', 'carta');
 	const front = createElement('div', 'face front');
 	const back = createElement('div', 'face back');
-
 	front.style.backgroundImage = `url('./css/image/${personagens}.png')`;
-
 	carta.appendChild(front);
+	back.style.backgroundImage = selectTypeCardNow(selectTypeCard);
 	carta.appendChild(back);
-
-	carta.addEventListener('click', reveleCarta);
-	carta.setAttribute('data-personagens', personagens)
-	
-
+  carta.addEventListener('click', reveleCarta);
+	carta.setAttribute('data-personagens', personagens);
 	return carta;
 }
 
-const loadGame = () => {
-
+const loadGame = (selectTypeCard) => {
 	const duplicatepersonagens = [ ...personagens, ...personagens]
-
 	const shufflyArray = duplicatepersonagens.sort(() => Math.random() - 0.5);
-
 	shufflyArray.forEach((personagens) => {
-		const carta = createCarta(personagens);
+		const carta = createCarta(personagens, selectTypeCard);
 		tabuleiro.appendChild(carta);
 	});
 }
-loadGame();
